a278142f1cd0750f1aafba39d2adf03794754c95
Silent-Samurai <silentsamurai@live.com>
2016-05-12 22:11:34 +1000

    Fixed output.

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -54,14 +54,11 @@ namespace SudokuSolver
             else
             {
                 lSudoku.Draw();
-
-                Console.WriteLine(lSudoku.ToString());
             }
 
             Console.WriteLine("\nTime taken - {0}ms", Timer.ElapsedMilliseconds);
             Console.WriteLine("{0} moves attempted.", lBT.CheckCount);
 
-            Console.WriteLine(lSudoku.ToString());
             Console.ReadKey();
         }
     }
